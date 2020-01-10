e92aa29b7072bf40c7ec924fdf3224fd74e5f191
pruthvi <pks9650@gmail.com>
2020-01-09 21:31:03 -0700

    started with creating the package

--- src/location_monitor.cpp
+++ src/location_monitor.cpp
@@ -1,1 +1,17 @@
-// pass
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"  //nav_msgs is the package (but odom is the topic)
+
+void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg){
+  ROS_INFO("x: %f. y: %f", ???, ???);
+}
+
+
+int main(int argc, char** argv){
+
+  ros::init(argc, argv, "location_monitor");
+  ros::NodeHandle nh;
+  ros::Subscriber = nh.subscribe("odom", 10, OdomCallback);
+  ros::spin();
+  return 0;
+
+}
